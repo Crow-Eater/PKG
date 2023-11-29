3f44c04dacd28442f5ac3e0c833d8e4fb3411909
Crow-Eater <86649818+Crow-Eater@users.noreply.github.com>
2023-11-29 21:58:31 +0300

    Add files via upload

---
+++ lab4/lab4-release/release/moc_mainwindow.cpp
@@ -0,0 +1,220 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../lab4_code/mainwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "refresh",
+    "",
+    "refreshImageCircle",
+    "valueInCircleChanged",
+    "refreshImageLine",
+    "valueInLinesChanged",
+    "valueInStepChanged",
+    "refreshImageStepLine",
+    "valueInDDAChanged",
+    "refreshImageDDALine"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
+    uint offsetsAndSizes[22];
+    char stringdata0[11];
+    char stringdata1[8];
+    char stringdata2[1];
+    char stringdata3[19];
+    char stringdata4[21];
+    char stringdata5[17];
+    char stringdata6[20];
+    char stringdata7[19];
+    char stringdata8[21];
+    char stringdata9[18];
+    char stringdata10[20];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 7),  // "refresh"
+        QT_MOC_LITERAL(19, 0),  // ""
+        QT_MOC_LITERAL(20, 18),  // "refreshImageCircle"
+        QT_MOC_LITERAL(39, 20),  // "valueInCircleChanged"
+        QT_MOC_LITERAL(60, 16),  // "refreshImageLine"
+        QT_MOC_LITERAL(77, 19),  // "valueInLinesChanged"
+        QT_MOC_LITERAL(97, 18),  // "valueInStepChanged"
+        QT_MOC_LITERAL(116, 20),  // "refreshImageStepLine"
+        QT_MOC_LITERAL(137, 17),  // "valueInDDAChanged"
+        QT_MOC_LITERAL(155, 19)   // "refreshImageDDALine"
+    },
+    "MainWindow",
+    "refresh",
+    "",
+    "refreshImageCircle",
+    "valueInCircleChanged",
+    "refreshImageLine",
+    "valueInLinesChanged",
+    "valueInStepChanged",
+    "refreshImageStepLine",
+    "valueInDDAChanged",
+    "refreshImageDDALine"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       9,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   68,    2, 0x0a,    1 /* Public */,
+       3,    0,   69,    2, 0x0a,    2 /* Public */,
+       4,    0,   70,    2, 0x0a,    3 /* Public */,
+       5,    0,   71,    2, 0x0a,    4 /* Public */,
+       6,    0,   72,    2, 0x0a,    5 /* Public */,
+       7,    0,   73,    2, 0x0a,    6 /* Public */,
+       8,    0,   74,    2, 0x0a,    7 /* Public */,
+       9,    0,   75,    2, 0x0a,    8 /* Public */,
+      10,    0,   76,    2, 0x0a,    9 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'refresh'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'refreshImageCircle'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'valueInCircleChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'refreshImageLine'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'valueInLinesChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'valueInStepChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'refreshImageStepLine'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'valueInDDAChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'refreshImageDDALine'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->refresh(); break;
+        case 1: _t->refreshImageCircle(); break;
+        case 2: _t->valueInCircleChanged(); break;
+        case 3: _t->refreshImageLine(); break;
+        case 4: _t->valueInLinesChanged(); break;
+        case 5: _t->valueInStepChanged(); break;
+        case 6: _t->refreshImageStepLine(); break;
+        case 7: _t->valueInDDAChanged(); break;
+        case 8: _t->refreshImageDDALine(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 9)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 9;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 9)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 9;
+    }
+    return _id;
+}
+QT_WARNING_POP
