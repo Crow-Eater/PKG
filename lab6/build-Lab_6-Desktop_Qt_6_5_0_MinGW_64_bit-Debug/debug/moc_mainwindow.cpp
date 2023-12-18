951014081af77275b5d58afbb96b7ee4178f151c
Crow-Eater <86649818+Crow-Eater@users.noreply.github.com>
2023-12-18 08:55:01 +0300

    Add files via upload

--- lab6/build-Lab_6-Desktop_Qt_6_5_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ lab6/build-Lab_6-Desktop_Qt_6_5_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../Lab_6_code/mainwindow.h"
+#include "../../Code/mainwindow.h"
 #include <QtGui/qtextcursor.h>
 #include <QScreen>
 #include <QtCore/qmetatype.h>
